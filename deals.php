eb23c72145821034aec792fe4108039c63142381
eric <shamaloveric@gmail.com>
2024-12-29 00:09:13 -0500

    Update deals.php

--- deals.php
+++ deals.php
@@ -1,23 +1,18 @@
-<?
-    $host = '127.0.0.1'; // Use '127.0.0.1' for TCP/IP connection
-    $dbname = 'buytofill';
-    $username = 'admin_jasdgfasfdafes';
-    $password = 'wZR}v&xg=S0Fsadwa3213damn';
-    
+<?  
     try {
-        // Create a PDO instance (connect to the database)
-        $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Enable exceptions for errors
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Fetch results as associative arrays
-            PDO::ATTR_EMULATE_PREPARES => false, // Disable emulated prepared statements
-        ];
-    
-        $pdo = new PDO($dsn, $username, $password, $options);
-    
+        $pdo = new PDO(
+            "mysql:host=127.0.0.1;dbname=buytofill;charset=utf8mb4", 
+            "admin_jasdgfasfdafes", 
+            "wZR}v&xg=S0Fsadwa3213damn", 
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ]
+        );
+
         echo "Connected successfully using PDO!";
     } catch (PDOException $e) {
-        // Handle connection error
         die("Connection failed: " . $e->getMessage());
     }
     exit;
