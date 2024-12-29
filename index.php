7dc748ed2ed0337603d9b6e125325d7bee3b0417
eric <shamaloveric@gmail.com>
2024-12-28 23:14:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
     #require 'assets/helper.php';
 
     session_start();
+
+    echo "Starting script...<br>";
+
     $host = 'localhost';
     $username = 'admin_jasdgfasfdafes';
     $password = 'wZR}v&xg=S0Fsadwa3213damn';
     $database = 'buytofill';
+
+    echo "Initializing connection...<br>";
+
     $conn = new mysqli($host, $username, $password, $database);
+
+    echo "After connection attempt...<br>";
 
     if ($conn->connect_error) {
         die("Connection failed: (" . $conn->connect_errno . ") " . $conn->connect_error);
     }
 
-    echo "Connected successfully!";
+    echo "Connected successfully!<br>";
     $conn->close();
+
+    echo "Connection closed successfully.<br>";
     exit;
+
 
     
     if(isset($_SESSION['role'])){
