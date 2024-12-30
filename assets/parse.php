36917a824eda562831b9c75abdf9d4f7c335a117
eric <shamaloveric@gmail.com>
2024-12-30 11:11:44 -0500

    Update

--- assets/parse.php
+++ assets/parse.php
@@ -1,6 +1,6 @@
 <?
     $input = file_get_contents('php://stdin');
-    file_put_contents('email.php', $input);
+    file_put_contents('email_log.txt', $input);
     #change to pdo
 
     /*$data = file_get_contents("php://stdin");
