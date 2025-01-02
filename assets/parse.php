ec1cc869d7b1364930cc2056e26ae3ab9d821628
eric <shamaloveric@gmail.com>
2025-01-02 10:50:49 -0500

    Update parse.php

--- assets/parse.php
+++ assets/parse.php
@@ -5,7 +5,7 @@
     #Supports Google
     #Add support for yahoo, outlook, icloud
     #Add support for manually forwarded emails
-    file_put_contents('email_log.txt', $data);
+    file_put_contents('email_log.txt', $sender);
     if($sender == "<forwarding-noreply@google.com>"){
         $ch = curl_init(str_replace('mail-settings.google', 'mail.google', preg_match('/https:\/\/mail-settings\.google\.com\/mail\/vf-[^\s"]+/i', $data, $m) ? $m[0] : ''));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
