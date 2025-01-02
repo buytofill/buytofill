6d8da9689adedf400587b38611fefdab93234c92
eric <shamaloveric@gmail.com>
2025-01-02 14:42:21 -0500

    Update parse.php

--- assets/parse.php
+++ assets/parse.php
@@ -54,8 +54,8 @@
                 curl_setopt($ch, CURLOPT_HEADER, 0);
                 curl_setopt($ch, CURLOPT_NOBODY, 0);
                 $clipped = json_decode(curl_exec($ch));
-                file_put_contents("email_log.txt", $clipped, FILE_APPEND);
-                $orderContents = $clipped->order->items;
+                file_put_contents("email_log.txt", print_r($clipped), FILE_APPEND);
+                #$orderContents = $clipped->order->items;
                 if($step == 3){
                     file_put_contents("email_log.txt", print_r(substr($v,strpos($v,'vt')+3,36)."; SID;", 1), FILE_APPEND);
                     file_put_contents("email_log.txt", print_r($orderContents, 1), FILE_APPEND);
