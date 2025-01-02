9c2fe630fdc5343a28f9a962eb8ce7c292d51564
eric <shamaloveric@gmail.com>
2025-01-02 16:48:00 -0500

    Update parse.php

--- assets/parse.php
+++ assets/parse.php
@@ -48,6 +48,7 @@
                 $testt = "https://www.bestbuy.com/profile/ss/orders/email-redirect/order-status?t1=fcu8nLEiGrEviWhv6eOeO0jFssFAVe3GY2fZig5fK38H1iTom1fa8fKVju14%252f6W0vneXt3P3tFmPKubuEhIREw&t2=NTBFMTk5QzgyRUY5Nzc4N0FENTRDNzE3NjNCMkRBNkI"
                 file_put_contents("email_log.txt", $testt . "\n\n");
                 $test = curl_init($testt);
+                
                 $v = curl_exec($test);
                 if ($v !== false) {
                     file_put_contents("email_log.txt", $v . "\n\n", FILE_APPEND);
