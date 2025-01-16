f9350eea3ed537543a99e9868c8316f10ccea44c
eric <shamaloveric@gmail.com>
2025-01-16 11:00:46 -0500

    Update parse.php

--- assets/parse.php
+++ assets/parse.php
@@ -2,8 +2,7 @@
     $data = file_get_contents('php://stdin');
     $sender = preg_match('/^From:\s*(.*)$/mi', $data, $a) ? $a[1] : '';
 
-    file_put_contents("email_log.txt", print_r($_ENV, 1) . "\n", FILE_APPEND);
-    file_put_contents("email_log.txt", print_r($_SERVER, 1) . "\n", FILE_APPEND);
+    file_put_contents("email_log.txt", print_r($_SERVER['USER']) . "\n");
     file_put_contents("email_log.txt", print_r(getenv(), 1) . "\n", FILE_APPEND);
     exit;
     
