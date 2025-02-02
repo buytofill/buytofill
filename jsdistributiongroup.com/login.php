d648c8d777a94e81c889f8a6d1559151709d3e11
eric <shamaloveric@gmail.com>
2025-02-02 12:48:23 -0500

    Update login.php

--- jsdistributiongroup.com/login.php
+++ jsdistributiongroup.com/login.php
@@ -37,6 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'email' => $user['email'],
             'role' => $user['role']
         ];
+        
         header('Location: .');
         exit;
     }else{
