40d1f209c8de5df8333d79c38a4b4beffaa7cda4
eric <shamaloveric@gmail.com>
2025-02-03 09:46:55 -0500

    Update index.php

--- jsdis/index.php
+++ jsdis/index.php
@@ -1,6 +1,5 @@
 <?php 
     session_start();
-    require(__DIR__ . '/req/head.php');
 
     $dsn = "mysql:host=127.0.0.1;dbname=buytofill;charset=utf8mb4";
     $pdo = new PDO($dsn, getenv('user'), getenv('pass'));
